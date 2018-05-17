f4d5b299e37702ea14a044ce884192d57daa5e99
Matt C <mcompton2002@gmail.com>
2018-05-17 14:21:58 +0000

    changes

--- parking.py
+++ parking.py
@@ -1,5 +1,14 @@
 class check:
     def __init__(self):
         return
-    def eval(self):
-        return "oof"
+    def eval(self,name,email,grade,sports,intern,dual,disabilities,other_reason):
+        if grade == 9:
+            return "No pass for you. How can you even drive??"
+        elif grade == 10:
+            return "Logged the special reason below, you'll hear from administrators soon, hopefully."
+        elif grade == 11:
+            # qualifiers
+            return
+        elif grade == 12:
+            # conditions
+            return

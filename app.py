8240cb58f0deec728030e5e1d05dba6eeb868543
Matt C <mcompton2002@gmail.com>
2018-05-17 14:06:47 +0000

    Files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import parking
+
+app = Flask(__name__)
+
+f = open("html/index.html")
+homepage = f.read()
+f.close()
+
+nick= parking.check()
+# nick.eval()
+
+@app.route("/")
+def index():
+    return homepage
+
+@app.route("/check/")
+def check():
+    return nick.eval()
+
+app.run(host='0.0.0.0',port=80)
---
+++ parking.py
@@ -0,0 +1,5 @@
+class check:
+    def __init__(self):
+        return
+    def eval(self):
+        return "oof"

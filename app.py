acf900d61a87092781e314a9495b3cfc1c64fdbf
Matt C <mcompton2002@gmail.com>
2018-05-18 14:13:52 +0000

    For Kris

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ homepage = f.read()
 f.close()
 
 nick= parking.check()
-# nick.eval()
 
+# app.route is the url that the user will enter in the browser to get the contents of this function
 @app.route("/")
 def index():
     return homepage
 
+# if you were running this locally, you'd enter http://localhost:2107/check to get this
 @app.route("/check/")
 def check():
     return nick.eval()
 
+# host 0.0.0.0 means run according to the system's policy, port not being on 80 (standard url port) means I won't get rko'd by bot DDOS probably
 app.run(host='0.0.0.0',port=2107)

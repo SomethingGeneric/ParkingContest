669cc4e1da0ce1227561ad5a233dce23cc848ffb
kketg <39269628+kketg@users.noreply.github.com>
2018-05-21 13:23:53 -0400

    Update app.py
    ffgsfd

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def index():
 # if you were running this locally, you'd enter http://localhost:2107/check to get this
   #string,string,int,string,boolean,boolean,boolean,string
 #/<name>/<id>/<grade>/<sports>/<internship>/<dual>/<disabilities>/<otherreason>
-@app.route("/check/<name>/<Id>/<grade>/<sports>/<internship>/<dual>/<disabilities>/<otherreason>")
-def check(name,Id,grade,sports,internship,dual,disabilities,otherreason):
-    stud = student(name,Id,grade,sports,internship,dual,disabilities,otherreason)
+@app.route("/check/<name>/<Id>/<grade>/<sports>/<internship>/<dual>/<disabilities>/<distance>/<otherreason>")
+def check(name,Id,grade,sports,internship,dual,disabilities,distance,otherreason):
+    stud = student(name,Id,grade,sports,internship,dual,disabilities,distance,otherreason)
     return nick.eval(stud)
 
 # host 0.0.0.0 means run according to the system's policy, port not being on 80 (standard url port) means I won't get rko'd by bot DDOS probably

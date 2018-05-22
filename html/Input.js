f802efc4625abe6c96bcce38e7f7d4771474add1
kketg <39269628+kketg@users.noreply.github.com>
2018-05-22 00:51:35 -0400

    testy

--- html/Input.js
+++ html/Input.js
@@ -1,6 +1,19 @@
 nameTextBox = document.getElementById("nameBox");
 IDTextBox = document.getElementById("idBox");
+gradeTextBox = document.getElementById("gradeBox");
+sportsCheckBox = document.getElementById("sportsBox");
+internCheckBox = document.getElementById("internBox");
+dualCheckBox = document.getElementById("dualBox");
+disabilitiesCheckBox = document.getElementById("disabilitiesBox");
+otherCheckBox = document.getElementById("otherBox");
+distanceTextBox = document.getElementById("distBox");
+SubmitButton = document.getElementById("SubmitButton");
 
-function register(name, id, grade, sports, intern, dual, disabilities, distance, otherReason){
-  
+SubmitButton.addEventListener("click", OnSubmitClick());
+
+
+function OnSubmitClick(){
+    window.open('0.0.0.0:2107/check/' + nameTextBox.value + '/' + IDTextBox.value + '/' + gradeTextBox.value + '/' + sportsCheckBox.value + '/' + internCheckBox.value + '/' + dualCheckBox.value + '/' + disabilitiesCheckBox.value + '/' + distanceTextBox.value + '/' + otherCheckBox, '_blank');
+    alert("hah lol kys");
 }
+

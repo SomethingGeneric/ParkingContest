03cfa6a0d7ba53d6ab32b7d9a1ecc58322eb933e
kketg <39269628+kketg@users.noreply.github.com>
2018-05-21 22:58:52 -0400

    thing

--- html/Input.js
+++ html/Input.js
@@ -1,4 +1,5 @@
-
+nameTextBox = document.getElementById("nameBox");
+IDTextBox = document.getElementById("idBox");
 
 function register(name, id, grade, sports, intern, dual, disabilities, distance, otherReason){
   

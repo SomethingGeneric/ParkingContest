f29ec502f6c6754465c6ef79abf1240bda518ee1
kketg <39269628+kketg@users.noreply.github.com>
2018-05-25 15:38:19 -0400

    fyf

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def check(name,Id,grade,sports,internship,dual,disabilities,distance,otherreason
     site = serve_raw("return")
     site = site.replace("$result",from_kris)
     return site
-    heff("heff.heck",stud.name + ", " + str(stud.ID) + ", " + str(stud.grade) + ", " + stud.sports + ", " + stud.internship + ", " + stud.dual + ", " + stud.disabilities + ", " + str(stud.distance) + ", " + stud.otherreason)
+    heff("heff.heck",stud.name + ", " + str(stud.ID) + ", " + str(stud.grade) + ", " + str(stud.sports) + ", " + str(stud.internship) + ", " + str(stud.dual) + ", " + str(stud.disabilities) + ", " + str(stud.distance) + ", " + str(stud.otherreason))
 
 def bFS(string):
     if string == "true":

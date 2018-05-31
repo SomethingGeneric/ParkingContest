0afebe2a55fc4747a9cc94b13f2665514e50a563
SomethingGeneric <mcompton2002@gmail.com>
2018-05-31 10:06:50 -0400

    Update email.py

--- email.py
+++ email.py
@@ -1,4 +1,4 @@
-from gmail import GMail,Message
+from gmail import *
 oof = GMail('Demo User <parkingcontestfocs@gmail.com>','thisisastrongpassword')
 msg = Message('Test Message',to='Matt Compton <mcompton2002@gmail.com>',text='Hello')
 oof.send(msg)

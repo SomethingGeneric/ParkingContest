3160b96fca9e3abca2ad5a9ddf2c61ec6daba1d4
SomethingGeneric <mcompton2002@gmail.com>
2018-05-31 10:14:42 -0400

    Update heck.py

--- heck.py
+++ heck.py
@@ -1,4 +1,14 @@
 from gmail import *
-oof = GMail('Demo User <parkingcontestfocs@gmail.com>','thisisastrongpassword')
-msg = Message('Test Message',to='Matt Compton <mcompton2002@gmail.com>',text='Hello')
-oof.send(msg)
+
+class heff:
+  def __init__(self):
+    self.oof = GMail('Demo User <parkingcontestfocs@gmail.com>','thisisastrongpassword')
+  def set_msg(self,recv,subj,cont):
+    self.msg = Message(subj,to=recv,text=cont)
+  def send(self):
+    self.oof.send(self.msg)
+    
+if __name__ == "__main__":
+  e = heff()
+  e.set_msg('mcompton2002@gmail.com','Hello?','Hello world?')
+  e.send()

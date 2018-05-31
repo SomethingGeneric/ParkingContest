c0c019e5c60063bb31b49f4c87eb45644ea7ddca
SomethingGeneric <mcompton2002@gmail.com>
2018-05-31 10:06:27 -0400

    Update email.py

--- email.py
+++ email.py
@@ -1,21 +1,4 @@
-# Python code to illustrate Sending mail from 
-# your Gmail account 
-import smtplib
- 
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
- 
-# start TLS for security
-s.starttls()
- 
-# Authentication
-s.login("parkingcontestfocs@gmail.com", "thisisastrongpassword")
- 
-# message to be sent
-message = "Message_you_need_to_send"
- 
-# sending the mail
-s.sendmail("parkingcontestfocs@gmail.com", "mcompton2002@gmail.com", message)
- 
-# terminating the session
-s.quit()
+from gmail import GMail,Message
+oof = GMail('Demo User <parkingcontestfocs@gmail.com>','thisisastrongpassword')
+msg = Message('Test Message',to='Matt Compton <mcompton2002@gmail.com>',text='Hello')
+oof.send(msg)

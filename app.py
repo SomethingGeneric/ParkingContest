a3be79aa8ebaa62483bcb7ca1ac201ea7ddb4891
kketg <39269628+kketg@users.noreply.github.com>
2018-05-31 22:46:01 -0400

    d

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def fourofour(code):
 
 @app.route("/space/<number>")
 def getSpaceInfo(number):
-    return linecache.getline(storageFN, number)
+    return linecache.getline(storageFN, int(number))
 
 
 # if you were running this locally, you'd enter http://localhost:2107/check to get this

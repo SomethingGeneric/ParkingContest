63729e8e01befc087d6b3c427d9d2fafbd3594d4
BreadStick1 <39269434+BreadStick1@users.noreply.github.com>
2018-06-04 10:05:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ def fourofour(code):
 def fouroneo(code):
     return "410: File Deleted <a href='http://home.mattcompton.me:2107/.>Click Here to go Back</a>"
 
+@app.errorhandle*(403)
+def fourothree(code):
+    return "403: Not allowed to enter <a href='http://home.mattcompteon.me:2107/.>Click Here to go Back</a>
+
 @app.route("/space/<number>")
 def getSpaceInfo(number):
     x = linecache.getline(storageFN, int(number) + 1)
